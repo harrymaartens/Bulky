5eda4f0df8db5c770b102ed726cd8c2473940e87
Harry Maartens <hmaartens@hotmail.com>
2024-05-27 23:19:54 +0200

    Section 15: User Management - Display Roles

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -23,6 +23,9 @@ namespace BulkyBook.Models
 
         [ForeignKey("CompanyId")]
         [ValidateNever]
-        public Company Company { get; set; }
+        public Company? Company { get; set; }
+
+        [NotMapped]
+        public string Role {  get; set; }
     }
 }
---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,88 @@
+﻿using BulkyBook.DataAcces.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+               
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+            // Hier haal je de mapping table op. Voeg dit toe in ApplicationUser in Models: [NotMapped] public string Role { get; set; }
+            // dbo.AspNetUserRoles wordt UserRoles            
+            var userRoles = _db.UserRoles.ToList();
+
+            // dbo.AspNetRoles wordt Roles table
+            var roles = _db.Roles.ToList();
+
+            foreach (var user in objUserList) 
+            {
+                // Hier halen we het RoleId op
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+
+                // Hier halen we de Name op
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
+
+
+            return Json(new { data = objUserList });
+        }
+
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
+        {
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+
+            if (objFromDb != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                // User is currently locked and we need to unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
