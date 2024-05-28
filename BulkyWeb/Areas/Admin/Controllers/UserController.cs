01ca968fea3fe989995ca7c4db2529c9d4b74085
Harry Maartens <hmaartens@hotmail.com>
2024-05-28 15:49:00 +0200

    Assignment 4 not working

--- BulkyWeb/Areas/Admin/Controllers/UserController.cs
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -25,6 +25,30 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult RoleManagement(string userId)
+        {
+            string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+
+            RoleManagmentVM RoleVM = new RoleManagmentVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _db.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _db.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+
+            RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+            return View(RoleVM);
+        }
                
 
         #region API CALLS
